10ccfe0891b94635437e43651adfbf64a1c610ad
Mutestock <doublenegative@protonmail.com>
2021-05-23 15:57:41 +0200

    backend base

--- data_population/tests/test_data_population.py
+++ data_population/tests/test_data_population.py
@@ -1,5 +1,23 @@
-from data_population import __version__
+import importlib
+import unittest
+import os
+
+SRC_PATH = "../src"
+
+connections = importlib.import_module("connection", SRC_PATH)
+environment = importlib.import_module("misc.environment", SRC_PATH)
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+class TestMisc(unittest.TestCase):
+    def test_environment_variable_file_found(self):
+        self.assertTrue(os.path.isfile(environment.ENVIRONMENT_VARIABLE_FILE))
+
+class TestMongo(unittest.TestCase):
+    def test_mongo_connection(self):
+        connections.mongo_connection.make_mongo_pool()
+        
+class TestPostgres(unittest.TestCase):
+    def test_postgres_connection(self):
+        connection = connections.postgres_connection.make_pg_pool()
+        cursor = connection.cursor()
+        self.assertTrue(cursor is not None)
