e1357e4910a86725de9b60866199bb15a60c6274
Mutestock <doublenegative@protonmail.com>
2021-05-26 00:00:11 +0200

    mongo people finished. One left ...

--- data_population/src/logic/mongo_populate.pyx
+++ data_population/src/logic/mongo_populate.pyx
@@ -13,31 +13,27 @@ import pandas as pd
 import os
 import time
 
+
 cpdef generate_phone_number():
     cdef str prefix = "#?"
     cdef int part_one = random.randint(10,99)
     cdef int part_two = random.randint(100,999)
-    cdef str full = prefix + "-" + str(part_one) + "-" + str(part_one) + "-" + str(part_two)
-    
-    return full
+    return  prefix + "-" + str(part_one) + "-" + str(part_one) + "-" + str(part_two)    
+
 
 cpdef generate_email(df):
-    cdef int random_index
-    cdef str suffix
-    cdef str prefix = ''.join(df.sample())
-    
-    random_index = random.randint(0, len(EMAIL_DOMAIN_NAMES) -1)
-    suffix = EMAIL_DOMAIN_NAMES[random_index]
+    cdef int random_index = random.randint(0, len(EMAIL_DOMAIN_NAMES) -1)
+    cdef str suffix = EMAIL_DOMAIN_NAMES[random_index]
+    cdef str prefix = ''.join(df.sample().iloc[0])
     return f"{prefix}@{suffix}"
 
+
 cpdef grab_first_name(df):
-    cdef str first_name = str(df.sample().iloc[0])
-    return first_name
+    return str(df.sample().iloc[0])
 
 
 cpdef grab_last_name(df):
-    cdef str last_name = str(df.sample().iloc[0])
-    return last_name
+    return str(df.sample().iloc[0])
 
 
 cpdef generate_people():
@@ -62,7 +58,7 @@ cpdef generate_people():
     time_start = time.time()
     print(f"{datetime.now().time()} - MongoDB: Populating people. {anime_dir_length} entries...")
     for count, filename in enumerate(os.listdir(ANIME_DIR)):
-        if count % 1000 == 0:
+        if count % 2000 == 0:
             print(f"{datetime.now().time()} - MongoDB: {count} / {anime_dir_length} completed ...")
         collection.insert_one({
             "first_name": grab_first_name(first_names_df),
@@ -73,23 +69,20 @@ cpdef generate_people():
         })
     print(f"{datetime.now().time()} - MongoDB: Done in {time_start - time.time()} seconds")
 
+
 cpdef generate_universities():
     cdef collection = get_mongo_collection("universities")
-
     cdef df = pd.read_csv(DATASETS.get("universities"))
     df.reset_index(inplace=True)
     cdef data_dict = df.to_dict("records")
 
     print(f"{datetime.now().time()} - MongoDB: Flushing universities...")
-
     collection.drop()
 
     print(f"{datetime.now().time()} - MongoDB: Populating universities...")
-
     collection.insert_many(data_dict)
 
     print(f"{datetime.now().time()} - MongoDB: Done")
-
 
 
 cpdef generate_locations():
